f69b77ed9e1f59a9b8dcf158c62eb1462ba484f1
Michael Wood <michael.wood@cabinpressure.co.uk>
2023-11-07 13:00:55 +0000

    Gone down an interesting, but bad, route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,74 @@
 import { useState } from 'react'
+import imgS from './assets/subjugated.svg';
+import imgE from './assets/empire.svg';
+import imgR from './assets/rebel.svg';
+import imgN from './assets/neutral.svg';
+import iTb from './assets/triangle--blue.svg';
+import iSb from './assets/square--blue.svg';
+import iCb from './assets/circle--blue.svg';
+import iTo from './assets/triangle--orange.svg';
+import iSo from './assets/square--orange.svg';
+import iCo from './assets/circle--orange.svg';
+import { galaxyJSON } from './galaxy.js';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [galaxy, setGalaxy] = useState(galaxyJSON)
   
-  return (
 
-    <div id="cnt">
+	const Galaxy = () => {
+		return (
+			<div id="galaxy">
+				<div id="board">#board</div>
+				{ galaxy.map((system,index) => <System key={index} system={system} /> ) }
+			</div>
+		)
+	};
 
-    <ul class="systems">
-      <li>
-        <ul>
-          <li data-slot="1" data-left="o3" data-always="1"><span class="hdr">Coruscant<span class="slot"></span></span></li>
-          <li data-slot="1" data-left="o3"><span class="hdr">Alderaan<span class="slot"></span></span></li>
-          <li data-slot="2" data-left="b3" data-right="o1"><span class="hdr">Cato Neimoidia<span class="slot"></span></span></li>
-          <li data-slot="3" data-left="b3" data-right="b4"><span class="hdr">Corellia<span class="slot"></span></span></li>
-        </ul>
-      </li>
-      <li>
-        <ul>
-          <li data-slot="2" data-left="b1" data-right="o1"><span class="hdr">Ord Mantell<span class="slot"></span></span></li>
-          <li class="remote"><span class="hdr">Ilum<span class="slot"></span></span></li>
-          <li class="remote"><span class="hdr">Dantooine<span class="slot"></span></span></li>
-          <li data-slot="2" data-left="b3" data-right="o4"><span class="hdr">Mygeeto<span class="slot"></span></span></li>
-        </ul>
-      </li>
-      <li>
-        <ul>
-          <li data-slot="1" data-left="o3" data-right="b3"><span class="hdr">Mandalore<span class="slot"></span></span></li>
-          <li class="remote"><span class="hdr">Dathomir<span class="slot"></span></span></li>
-          <li data-slot="1" data-left="o3"><span class="hdr">Malastare<span class="slot"></span></span></li>
-          <li data-slot="1" data-left="o3" data-right="o3"><span class="hdr">Kashyyyk<span class="slot"></span></span></li>
-        </ul>
-      </li>
-      <li>
-        <ul>
-          <li data-slot="2" data-left="b3" data-right="b1"><span class="hdr">Mustafar<span class="slot"></span></span></li>
-          <li class="remote"><span class="hdr">Hoth<span class="slot"></span></span></li>
-          <li class="remote"><span class="hdr">Endor<span class="slot"></span></span></li>
-          <li data-slot="1" data-left="o1"><span class="hdr">Bespin<span class="slot"></span></span></li>
-        </ul>
-      </li>
-      <li>
-        <ul>
-          <li data-slot="3" data-left="b3" data-right="b4"><span class="hdr">Mon Calamari<span class="slot"></span></span></li>
-          <li data-slot="1" data-left="o1"><span class="hdr">Felucia<span class="slot"></span></span></li>
-          <li class="remote"><span class="hdr">Yavin<span class="slot"></span></span></li>
-          <li data-slot="1" data-left="o1"><span class="hdr">Saleucami<span class="slot"></span></span></li>
-        </ul>
-      </li>
-      <li>
-        <ul>
-          <li data-slot="1" data-left="o3" data-right="b3"><span class="hdr">Nal Hutta<span class="slot"></span></span></li>
-          <li data-slot="2" data-left="b1"><span class="hdr">Toydaria<span class="slot"></span></span></li>
-          <li data-slot="1" data-left="o3"><span class="hdr">Kessel<span class="slot"></span></span></li>
-          <li data-slot="1" data-left="o1"><span class="hdr">Bothawui<span class="slot"></span></span></li>
-        </ul>
-      </li>
-      <li>
-        <ul>
-          <li data-slot="1" data-left="o3"><span class="hdr">Rodia<span class="slot"></span></span></li>
-          <li class="remote"><span class="hdr">Tatooine<span class="slot"></span></span></li>
-          <li data-slot="2" data-left="b1" data-right="o4"><span class="hdr">Geonosis<span class="slot"></span></span></li>
-          <li data-slot="1" data-left="o3"><span class="hdr">Ryloth<span class="slot"></span></span></li>
-        </ul>
-      </li>
-      <li>
-        <ul>
-          <li data-slot="2" data-left="o3" data-right="o4"><span class="hdr">Sullust<span class="slot"></span></span></li>
-          <li class="remote"><span class="hdr">Dagobah<span class="slot"></span></span></li>
-          <li data-slot="1" data-left="o3" data-right="b3"><span class="hdr">Naboo<span class="slot"></span></span></li>
-          <li data-slot="3" data-left="b1" data-right="b4"><span class="hdr">Utapau<span class="slot"></span></span></li>
-        </ul>
-      </li>
+	const System = (props) => {
+		const internalName = `system--${props.system.filter((planet,index) => index === 0)[0].name.replace(/\s+/g, '-').toLowerCase()}`;
+		return (
+			<div className={internalName}>
+				{
+					props.system.map((planet,index) => <Planet key={index} name={planet.name} slot={planet.slot} left={planet.left} right={planet.right} always={planet.always} remote={planet.remote}  /> )
+				}
+			</div>
+		)
+	};
 
-      <li>
-        <ul>
-          <li data-slot="1" data-left="b3" data-right="o3" data-always="4"><span class="hdr">Rebel Base<span class="slot"></span></span></li>
-        </ul>
-      </li>
-    </ul>
+	const Planet = (props) => {
+		const internalName = `planet--${props.name.replace(/\s+/g, '-').toLowerCase()}`;
+		
+		
+		return (
+			<div className={internalName}>
+				<img src={imgS} />
+				<span className="hdr">{props.name}</span>
+				{ props.left && 
+				<>
+					{props.left[1] === 'triangle' && (props.left[0] === 'orange'?<img src={iTo} />:<img src={iTb} />)}
+					{props.left[1] === 'square'   && (props.left[0] === 'orange'?<img src={iSo} />:<img src={iSb} />)}
+					{props.left[1] === 'circle'   && (props.left[0] === 'orange'?<img src={iCo} />:<img src={iCb} />)}
+				</>
+				}
+				{ props.right && 
+				<>
+					{props.right[1] === 'triangle' && (props.right[0] === 'orange'?<img src={iTo} />:<img src={iTb} />)}
+					{props.right[1] === 'square'   && (props.right[0] === 'orange'?<img src={iSo} />:<img src={iSb} />)}
+					{props.right[1] === 'circle'   && (props.right[0] === 'orange'?<img src={iCo} />:<img src={iCb} />)}
+				</>
+				}
+				<ul className="icons">
+					<li><img src={imgE} /></li>
+					<li><img src={imgS} /></li>
+					<li><img src={imgN} /></li>
+					<li><img src={imgR} /></li>
+				</ul>
+			</div>
+		)
+	};
 
-
-    </div>
-  )
+	return <Galaxy />
 }
 
 export default App
---
+++ src/galaxy.js
@@ -0,0 +1,55 @@
+const galaxyJSON = [
+    [
+        { "slot": 1 , "left": ["orange","triangle"] , "always": true , "name": "Coruscant" },
+        { "slot": 1 , "left": ["orange","triangle"] , "name": "Alderaan" },
+        { "slot": 2 , "left": ["blue","triangle"] , "right": ["orange","circle"], "name": "Cato Neimoidia" },
+        { "slot": 3 , "left": ["blue","triangle"] , "right": ["blue","square"], "name": "Corellia" }
+    ],
+    [
+        { "slot": 2 , "left": ["blue","circle"] , "right": ["orange","circle"], "name": "Ord Mantell" },
+        { "remote": true , "name": "Ilum" },
+        { "remote": true , "name": "Dantooine" },
+        { "slot": 2 , "left": ["blue",3] , "right": ["orange","square"], "name": "Mygeeto" }
+    ],
+    [
+        { "slot": 1 , "left": ["orange","triangle"] , "right": ["blue","triangle"], "name": "Mandalore" },
+        { "remote": true , "name": "Dathomir" },
+        { "slot": 1 , "left": ["orange","triangle"], "name": "Malastare" },
+        { "slot": 1 , "left": ["orange","triangle"] , "right": ["orange","triangle"], "name": "Kashyyyk" }
+    ],
+    [
+        { "slot": 2 , "left": ["blue","triangle"] , "right": ["blue","circle"], "name": "Mustafar" },
+        { "remote": true , "name": "Hoth" },
+        { "remote": true , "name": "Endor" },
+        { "slot": 1 , "left": ["orange","circle"], "name": "Bespin" }
+    ],
+    [
+        { "slot": 3 , "left": ["blue","triangle"] , "right": ["blue","square"], "name": "Mon Calamari" },
+        { "slot": 1 , "left": ["orange","circle"], "name": "Felucia" },
+        { "remote": true , "name": "Yavin" },
+        { "slot": 1 , "left": ["orange","circle"], "name": "Saleucami" }
+    ] ,
+    [
+        { "slot": 1 , "left": ["orange","triangle"] , "right": ["blue","triangle"], "name": "Nal Hutta" },
+        { "slot": 2 , "left": ["blue","circle"], "name": "Toydaria" },
+        { "slot": 1 , "left": ["orange","triangle"], "name": "Kessel" },
+        { "slot": 1 , "left": ["orange","circle"], "name": "Bothawui" }
+    ] ,
+    [
+        { "slot": 2 , "left": ["blue","circle"] , "right": ["orange","square"], "name": "Geonosis" },
+        { "slot": 1 , "left": ["orange","triangle"], "name": "Rodia" },
+        { "remote": true , "name": "Tatooine" },
+        { "slot": 1 , "left": ["orange","triangle"], "name": "Ryloth" }
+    ] ,
+    [
+        { "slot": 1 , "left": ["orange","triangle"] , "right": ["blue","triangle"], "name": "Naboo" },
+        { "slot": 2 , "left": ["orange","triangle"] , "right": ["orange","square"], "name": "Sullust" },
+        { "remote": true , "name": "Dagobah" },
+        { "slot": 3 , "left": ["blue","circle"] , "right": ["blue","square"], "name": "Utapau" }
+    ],
+    [
+        { "slot": 1 , "left": ["blue","triangle"] , "right": ["orange","triangle"] , "always": true , "name": "Rebel Base" }
+    ]
+]
+
+export { galaxyJSON } ;
